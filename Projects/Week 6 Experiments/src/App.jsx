c6a9b8d5766b7ff457d7472966487e6611e1871b
kuladi03 <kuladi03@gmail.com>
2024-01-12 16:36:34 +0530

    Updated

--- Projects/Week 6 Experiments/src/App.jsx
+++ Projects/Week 6 Experiments/src/App.jsx
@@ -1,37 +1,69 @@
-import { useState } from 'react'
-import './App.css'
-
-
-
+import { useState } from "react"
+let GLOBAL_ID = 4;
 function App() {
+//   const [todos, setTodos] = useState([{
+//     id: 1,
+//     title: "Go to gym",
+//     description: "Need to hit the gym from 7-9PM"
+//   }, {
+//     id: 2,
+//     title: "Go to Clas",
+//     description: "Need to go to the class from 4-7 PM"
+//   }, {
+//     id: 3,
+//     title: "Eat foor",
+//     description: "Need to eat food from 2-4 PM"
+//   }])
+
+//   function addTodo() {
+//     setTodos([...todos, {
+//       id: GLOBAL_ID++,
+//       title: "new todo",
+//       description: "new todo desc"
+//     }])
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={addTodo}>Add todo</button>
+//       {todos.map((todo, index) => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
+//     </div>
+//   )
+// }
+
+// function Todo({title, description}) {
+//   return <div>
+//     <h1>
+//       {title}
+//     </h1>
+//     <h4>
+//       {description}
+//     </h4>
+//   </div>
+// }
+
 
   return (
     <div>
-      <HeaderWithButton />
-      <Headers title="Aditya2"/>
+      <CardWrapper>
+        Hi there
+      </CardWrapper>
+      <CardWrapper>
+        Hello there
+      </CardWrapper>
     </div>
   )
 }
 
-function HeaderWithButton(){
-  const [title,settitle] = useState("My name is Aditya")
-  function changetitle(){
-    settitle("My name is " + Math.random())
-  }
+function CardWrapper({children}){
   return (
-    <div>
-    <button onClick={changetitle}>Update the Title</button>
-      <Headers title={title}/>
+    <div style={{border : "2px background: #000;", padding: 20}}>
+      {children}
     </div>
   )
 }
 
-function Headers({title}){
-  return(
-    <div>
-      {title}
-    </div>
-  )
-}
+
+
 
 export default App
